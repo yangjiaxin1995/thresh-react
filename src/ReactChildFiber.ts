287f08ede4032be56a1f8d9a726777b87544eda4
Thresh <601426683@qq.com>
2022-07-08 23:03:20 +0800

    feat: 目录迁移

---
+++ src/ReactChildFiber.ts
@@ -0,0 +1,65 @@
+import { createFiber } from './ReactFiber';
+import { isArray, isStringOrNumber, Update } from './utils';
+
+// 协调（diff）
+// abc
+// bc
+export function reconcileChildren(wip: any, children: any) {
+  if (isStringOrNumber(children)) {
+    return;
+  }
+
+  const newChildren = isArray(children) ? children : [children];
+  // oldfiber的头结点
+  let oldFiber = wip.alternate?.child;
+  let previousNewFiber: any = null;
+  for (let i = 0; i < newChildren.length; i++) {
+    const newChild = newChildren[i];
+    if (newChild == null) {
+      continue;
+    }
+    const newFiber = createFiber(newChild, wip);
+    const same = sameNode(newFiber, oldFiber);
+
+    if (same) {
+      Object.assign(newFiber, {
+        stateNode: oldFiber.stateNode,
+        alternate: oldFiber,
+        flags: Update,
+      });
+    }
+
+    if (!same && oldFiber) {
+      deleteChild(wip, oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    if (previousNewFiber === null) {
+      // head node
+      wip.child = newFiber;
+    } else {
+      previousNewFiber.sibling = newFiber;
+    }
+
+    previousNewFiber = newFiber;
+  }
+}
+
+// 节点复用的条件：1. 同一层级下 2. 类型相同 3. key相同
+function sameNode(a: any, b: any) {
+  return a && b && a.type === b.type && a.key === b.key;
+}
+
+// 删除单个节点
+function deleteChild(returnFiber: any, childToDelete: any) {
+  // returnFiber.deletoins = [...]
+  const deletions = returnFiber.deletions;
+  if (deletions) {
+    returnFiber.deletions.push(childToDelete);
+  } else {
+    returnFiber.deletions = [childToDelete];
+  }
+}
--- src/ReactFiberReconciler.ts
+++ src/ReactFiberReconciler.ts
@@ -1,6 +1,6 @@
-import { createFiber } from './ReactFiber';
+import { reconcileChildren } from './ReactChildFiber';
 import { renderWithHooks } from './hooks';
-import { isArray, isStringOrNumber, Update, updateNode } from './utils';
+import { updateNode } from './utils';
 
 // 原生标签
 export function updateHostComponent(wip: any) {
@@ -49,67 +49,4 @@ export function updateFragmentComponent(wip: any) {
 export function updateHostTextComponent(wip: any) {
   // console.log('test host text component wip', wip, new Date().valueOf());
   wip.stateNode = document.createTextNode(wip.props.children);
-}
-
-// 删除单个节点
-function deleteChild(returnFiber: any, childToDelete: any) {
-  // returnFiber.deletoins = [...]
-  const deletions = returnFiber.deletions;
-  if (deletions) {
-    returnFiber.deletions.push(childToDelete);
-  } else {
-    returnFiber.deletions = [childToDelete];
-  }
-}
-
-// 协调（diff）
-// abc
-// bc
-function reconcileChildren(wip: any, children: any) {
-  if (isStringOrNumber(children)) {
-    return;
-  }
-
-  const newChildren = isArray(children) ? children : [children];
-  // oldfiber的头结点
-  let oldFiber = wip.alternate?.child;
-  let previousNewFiber = null;
-  for (let i = 0; i < newChildren.length; i++) {
-    const newChild = newChildren[i];
-    if (newChild == null) {
-      continue;
-    }
-    const newFiber = createFiber(newChild, wip);
-    const same = sameNode(newFiber, oldFiber);
-
-    if (same) {
-      Object.assign(newFiber, {
-        stateNode: oldFiber.stateNode,
-        alternate: oldFiber,
-        flags: Update,
-      });
-    }
-
-    if (!same && oldFiber) {
-      deleteChild(wip, oldFiber);
-    }
-
-    if (oldFiber) {
-      oldFiber = oldFiber.sibling;
-    }
-
-    if (previousNewFiber === null) {
-      // head node
-      wip.child = newFiber;
-    } else {
-      previousNewFiber.sibling = newFiber;
-    }
-
-    previousNewFiber = newFiber;
-  }
-}
-
-// 节点复用的条件：1. 同一层级下 2. 类型相同 3. key相同
-function sameNode(a: any, b: any) {
-  return a && b && a.type === b.type && a.key === b.key;
 }
